734dc480574b24aa20a448b3def55c18072852d3
Davidson Schaly <davidson.schaly@gmail.com>
2025-02-17 14:34:39 -0300

    feat - DeleteBranch Mediator logic

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Branches/DeleteBranch/DeleteBranchResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Ambev.DeveloperEvaluation.Application.Branches.DeleteBranch;
+
+/// <summary>
+/// Response model for DeleteBranchResponse operation
+/// </summary>
+public class DeleteBranchResponse
+{
+    /// <summary>
+    /// Indicates whether the deletion was successful
+    /// </summary>
+    public bool Success { get; set; }
+}
