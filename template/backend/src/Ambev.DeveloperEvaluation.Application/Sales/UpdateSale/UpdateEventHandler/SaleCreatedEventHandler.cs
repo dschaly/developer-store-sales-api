eeaeccddfecf5e26ac526c4ea86ca909d2fd8f91
Davidson Schaly <davidson.schaly@gmail.com>
2025-02-20 03:23:05 -0300

    feat - added Rebus Events

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/UpdateSale/UpdateEventHandler/SaleCreatedEventHandler.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+using Rebus.Handlers;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.UpdateSale.UpdateEventHandler;
+
+public class SaleUpdateEventHandler : IHandleMessages<SaleUpdatedEvent>
+{
+    private readonly ILogger<SaleUpdateEventHandler> _logger;
+
+    public SaleUpdateEventHandler(ILogger<SaleUpdateEventHandler> logger)
+    {
+        _logger = logger;
+        _logger.LogInformation("🔥 SaleUpdateEventHandler registered!");
+    }
+
+    public async Task Handle(SaleUpdatedEvent message)
+    {
+        _logger.LogInformation("✅ Sale Updated: Number={SaleNumber}, Amount=R${TotalAmount}, UpdatedAt={UpdateAt}",
+            message.SaleNumber, message.TotalAmount, message.UpdatedAt);
+
+        await Task.CompletedTask;
+    }
+}
